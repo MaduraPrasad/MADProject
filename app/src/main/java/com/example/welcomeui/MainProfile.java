3942345b85880bb9b8052c8ba8670dc19dc19296
MaduraPrasad <Madura.prasad44@gmail.com>
2020-08-17 01:21:42 +0530

    Code For UI

---
+++ app/src/main/java/com/example/welcomeui/MainProfile.java
@@ -0,0 +1,14 @@
+package com.example.welcomeui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_profile);
+    }
+}
